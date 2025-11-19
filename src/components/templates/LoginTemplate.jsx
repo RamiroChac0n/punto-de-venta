c1c1d3cf9744ef0dc171472ffe1e2024f2735b0b
Ramiro <ramiro20010202@gmail.com>
2025-11-19 17:20:45 -0600

    Culminando diseño

--- src/components/templates/LoginTemplate.jsx
+++ src/components/templates/LoginTemplate.jsx
@@ -1,37 +1,55 @@
-import {styled} from 'styled-components'
-import { Btnsave, InputText2, Linea, Title } from '../../index'
-import {v} from '../../styles/variables'
+import { styled } from "styled-components";
+import { Btnsave, InputText2, Linea, Title } from "../../index";
+import { v } from "../../styles/variables";
+import { Device } from "../../styles/breakpoints";
 
-export function LoginTemplate(){
-    return(
-        <Container>
-            <section className='contentCard'>
-                <div className='card'>
-                    <Title>
-                        Ingresar
-                    </Title>
-                    <form action="">
-                        <InputText2>
-                            <input className='form__field' placeholder='email' type='text'/>
-                        </InputText2>
-                        <InputText2>
-                            <input className='form__field' placeholder='contraseña' type='password'/>
-                            <Btnsave titulo='INGRESAR' bgcolor='#1CB0F6' color='255,255,255' width='100%'/>
-                        </InputText2>              
-                    </form>
-                    <Linea >
-                        <span>0</span>
-                    </Linea>
-                    <Btnsave titulo='Google' bgcolor='#ffff' icono={<v.iconogoogle />}/>
-                </div>
-            </section>
-        </Container>
-    )
+export function LoginTemplate() {
+  return (
+    <Container>
+      <div className="card">
+        <Title $paddingbottom="20px">Ingresar</Title>
+        <form action="">
+          <InputText2>
+            <input className="form__field" placeholder="email" type="text" />
+          </InputText2>
+          <InputText2>
+            <input
+              className="form__field"
+              placeholder="contraseña"
+              type="password"
+            />
+          </InputText2>
+          <Btnsave
+            titulo="INGRESAR"
+            bgcolor="#1CB0F6"
+            color="255,255,255"
+            width="100%"
+          />
+        </form>
+        <Linea>
+          <span>0</span>
+        </Linea>
+        <Btnsave titulo="Google" bgcolor="#ffff" icono={<v.iconogoogle />} />
+      </div>
+    </Container>
+  );
 }
 
 export const Container = styled.div`
-    height: 100vh;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  .card {
     display: flex;
+    flex-direction: column;
     justify-content: center;
-    align-items: center;
-`
+    height: 100%;
+    width: 100%;
+    margin: 20px;
+    @media ${Device.tablet} {
+      width: 400px;
+    }
+  }
+`;
