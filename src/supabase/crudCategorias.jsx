408ef14b36f7ce5d99203418fff2cbd9110a2650
Ramiro <ramiro20010202@gmail.com>
2025-11-22 21:52:26 -0600

    Condigurando el storage

--- src/supabase/crudCategorias.jsx
+++ src/supabase/crudCategorias.jsx
@@ -1,5 +1,15 @@
+import Swal from "sweetalert2";
 import { supabase } from "../index";
 
-export async function InsertarCategorias(p,file){
-    const {error, data} = await supabase.from('categorias').insert()
+export async function InsertarCategorias(p, file) {
+  const { error, data } = await supabase.rpc("insertar_categorias", p);
+  if (error) {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: error.message,
+    });
+  }
+
+  const nuevo_id = data;
 }
